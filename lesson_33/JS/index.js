03d29396711beedaeee88fe474375f370110f869
Sanzhar <sulsanzhar04@gmail.com>
2024-02-10 20:24:20 +0600

    создал функцию, которая отображает первоначальную html структуру

--- lesson_33/JS/index.js
+++ lesson_33/JS/index.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const form = document.querySelector('form');
+    const input = document.querySelector('input');
+    const btn = document.querySelector('button');
+    const container = document.getElementById('container');
+
+
+    function showHTMLElements() {
+        const country = document.createElement('div');
+        country.innerText = 'Страна:';
+        const p = document.createElement('p');
+        p.innerText = input.value;
+        country.append(p);
+
+        const border = document.createElement('ul');
+        border.innerText = 'Граничит с:';
+
+        container.append(country, border);
+    };
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        showHTMLElements();
+
+    })
 })
