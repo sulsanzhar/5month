b6c00a0f513da68194f0ddaaeb81381a81f2c97e
Sanzhar <sulsanzhar04@gmail.com>
2024-02-10 20:28:04 +0600

    функция которая запрашивает данные у сервера

--- lesson_33/JS/index.js
+++ lesson_33/JS/index.js
@@ -4,6 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const btn = document.querySelector('button');
     const container = document.getElementById('container');
 
+    const request = config => {
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", function () {
+            if (this.status >= 200 && this.status < 300) {
+                config.success(this.responseText);
+            } else {
+                config.error(this.status);
+            }
+        });
+        xhr.addEventListener('error', function () {
+            config.error('No internet');
+        });
+        xhr.addEventListener('timeout', function () {
+            config.error('Timeout');
+        });
+        xhr.open(config.method, config.url);
+        xhr.send();
+    }
 
     function showHTMLElements() {
         const country = document.createElement('div');
